041cd5b54fa668f0ab860a30042f620b2eef20c3
nuyeo <bluelayzxx@gmail.com>
2026-01-22 17:57:46 +0900

    submit

---
+++ bin/README.md
@@ -0,0 +1,26 @@
+## 📘 과제 설명 문서 (필독)
+
+과제의 전체 요구사항, 학습 의도, 평가 기준은 아래 **노션 문서**에 정리되어 있습니다.  
+구현 전 반드시 확인하십시오.
+
+👉 [**노션 과제 설명 문서**](https://myknow.notion.site/2ee406c3c48880728700eb33cf40c2cc?source=copy_link)
+
+
+## 🛠 IDE 사용 주의사항 (IntelliJ IDEA 기준)
+### JDK 버전
+- **JDK 21 LTS 사용 가능**
+- 단, **Java 8 문법만 사용**해야 합니다  
+  (교육 목적상 최신 문법 사용 금지)
+### 테스트 코드
+- `BankServiceTest.java`는 **절대 수정 금지**
+- 패키지명은 `bank`로 **main / test 동일**
+- 테스트가 인식되지 않을 경우:
+  - Maven 프로젝트 여부 확인
+  - JUnit5 의존성(pom.xml) 확인
+
+## 📌 제출 방식
+1.	본 Repository를 Fork
+2.	Fork한 Repository를 Clone
+3.	요구사항에 맞게 구현
+4.	모든 테스트 통과 확인
+5.	Pull Request 생성
--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -1,4 +1,45 @@
 package bank;
 
 public class Account {
+	
+	protected String owner;
+	protected int balance;
+
+	public Account(String owner) {
+		this.owner = owner;
+	}
+	
+	public Account(String owner, int balance) {
+		this.owner = owner;
+		if (balance > 0) {
+			this.balance = balance;
+		}
+	}
+	
+	void deposit(int amount) {
+		//TODO: 입금
+	    if (amount <= 0) {
+	        System.out.println("1원 이상 입금 가능합니다.");
+	        return;
+	    }
+	    
+	    this.balance += amount;
+	}
+	
+	void withdraw(int amount) {
+		//TODO: 출금
+	    if (amount <= 0) {
+	        System.out.println("1원 이상 출금 가능합니다.");
+	        return;
+	    }
+	    
+	    if (this.balance < amount) {
+	        System.out.println("잔액이 부족합니다.");
+	        return;
+	    }
+	    
+	    this.balance -= amount;
+	}
+	
+	
 }
